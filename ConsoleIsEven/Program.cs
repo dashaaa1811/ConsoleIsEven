478686633bbca79b56839cc0b4cdf36e004c14e4
Dasha <darabrueva65@gmail.com>
2025-03-08 11:22:22 +0300

    Добавьте файлы проекта.

---
+++ ConsoleIsEven/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleIsEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число a");
+            int num =int.Parse(Console.ReadLine());
+            Console.WriteLine(IsEven(num));
+        }
+         static bool IsEven(int a)
+        {
+            if(a%2==0) return true;
+            else return false;
+        }
+    }
+}
